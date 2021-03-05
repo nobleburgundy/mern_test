2d0d493ed3d32f2184dae64f0964c9f5817ae873
James Gould <nobleburgundy@gmail.com>
2021-03-04 21:43:11 -0600

    2nd commit

---
+++ client/build/precache-manifest.6f1561efd90c64d7a29bf9dfe745a3f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c76573dd8904dc0ebcf0f191a6f0467f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae9af76bebe867fb38ff",
+    "url": "/static/css/main.1ef92e19.chunk.css"
+  },
+  {
+    "revision": "dcd954fd859b7c60d919",
+    "url": "/static/js/2.e265699e.chunk.js"
+  },
+  {
+    "revision": "223be75f6809993b56a94be00d710bed",
+    "url": "/static/js/2.e265699e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae9af76bebe867fb38ff",
+    "url": "/static/js/main.c111c760.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
